a4df50c381e41f236a40a5ff406d9234bbc538ce
Zach0702 <50173310+Zach0702@users.noreply.github.com>
2019-12-01 16:48:37 -0500

    apitest

---
+++ PokeQuizWebAPI/PokemonServices/IPokemonService.cs
@@ -0,0 +1,13 @@
+﻿using PokeQuizWebAPI.Models.PokemonViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeQuizWebAPI.PokemonServices
+{
+    public interface IPokemonService
+    {
+        Task<PokemonResponse> MapPokemonInfo(int id);
+    }
+}
