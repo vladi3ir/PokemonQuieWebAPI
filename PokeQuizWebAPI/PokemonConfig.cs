7a00769be2bdaa7e11c10a8bdaa8c6058c310277
Vlad <42613754+vladi3ir@users.noreply.github.com>
2019-12-12 20:57:47 -0500

    Identity and Dapper are now configured to work with Azure

--- PokeQuizWebAPI/PokemonConfig.cs
+++ PokeQuizWebAPI/PokemonConfig.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace PokeQuizWebAPI
+﻿namespace PokeQuizWebAPI
 {
     public class PokemonConfig
     {
         public Database Database { get; set; }
     }
+
     public class Database
     {
         public string ConnectionString { get; set; }
