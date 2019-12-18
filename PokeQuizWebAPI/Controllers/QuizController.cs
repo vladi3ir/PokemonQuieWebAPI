8f9813ac9a26db10800b63388bd32ec521d1703d
Vlad <42613754+vladi3ir@users.noreply.github.com>
2019-12-18 18:51:39 -0500

    updated Front End App

--- PokeQuizWebAPI/Controllers/QuizController.cs
+++ PokeQuizWebAPI/Controllers/QuizController.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using PokeQuizWebAPI.CalculationsService;
 using PokeQuizWebAPI.Models.QuizModels;
 using PokeQuizWebAPI.PokemonServices;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace PokeQuizWebAPI.Controllers
@@ -55,7 +53,7 @@ namespace PokeQuizWebAPI.Controllers
 
                 var quizResults = await _quizFlow.SetQuizResults();
                 _pokemonUserSQLService.CreatePokemonUserData(quizResults);
-                return View("QuizResults",quizResults);
+                return View("QuizResults", quizResults);
 
             }
             return View(quizModel);
